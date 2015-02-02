f6f64a0673f09f0a09b5f641be6cfdc013dc6c4e
Marek Lewandowski <m.lewandowski@cksource.com>
2015-02-02 16:12:18 +0100

    Fixed paths given to the HTTP server.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 'use strict';
 
 var path = require( 'path' ),
-	sinonPath = path.join( require.resolve( 'sinon' ), '../../pkg/sinon.js' ).split( path.sep ).join( '/' ),
-	sinonIEPath = path.join( require.resolve( 'sinon' ), '../../pkg/sinon-ie.js' ).split( path.sep ).join( '/' );
+	sinonPath = path.join( require.resolve( 'sinon' ), '../../pkg/sinon.js' ),
+	sinonIEPath = path.join( require.resolve( 'sinon' ), '../../pkg/sinon-ie.js' );
 
 module.exports = {
 	name: 'bender-sinon',
@@ -19,10 +19,10 @@ module.exports = {
 		function build( data ) {
 			data.parts.push(
 				'<head>' +
-					'<script src="' + path.join( '/plugins', sinonPath ) + '"></script>' +
+					'<script src="/plugins/' + sinonPath.split( path.sep ).join( '/' ) + '"></script>' +
 					// IE8- need additional care to make timers and XHR work.
 					'<!--[if lte IE 8]>' +
-						'<script src="' + path.join( '/plugins', sinonIEPath ) + '"></script>' +
+						'<script src="/plugins/' + sinonIEPath.split( path.sep ).join( '/' ) + '"></script>' +
 					'<![endif]-->' +
 				'</head>'
 			);
